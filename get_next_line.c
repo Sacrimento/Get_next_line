274c5d2b8a4f90bd7733d62072ecdfe2188a1e52
Sacrimento <abouvero@student.42.fr>
2017-12-13 14:01:56 +0100

    Debugging, segfault remaining on the last line, last fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:24:23 by abouvero          #+#    #+#             */
-/*   Updated: 2017/12/11 18:56:02 by abouvero         ###   ########.fr       */
+/*   Updated: 2017/12/13 14:01:11 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void print_list(t_fd_list **list) {
+void print_list(t_fd_list **kr) {
+	t_fd_list **list = kr;
 	while (*list)
 	{
 		printf("[fd : %d] [str : \'%s\']\n", (*list)->fd, (*list)->str);
@@ -22,43 +23,50 @@ void print_list(t_fd_list **list) {
 
 void 		delete_curr(t_fd_list **list, t_fd_list *curr)
 {
-	while ((*list)->next)
+	t_fd_list *kr = *list;
+	while (kr->next)
 	{
-		if ((*list)->next == curr)
+		printf("lol\n");
+		if (kr->next->fd == curr->fd)
 		{
-			(*list)->next = curr->next;
+			if (curr->next)
+				kr->next = curr->next;
 			free(curr->str);
 			free(curr);
 			return ;
 		}
-		(*list) = (*list)->next;
+		kr = kr->next;
 	}
-	if (!((*list)->next))
-	{
-		free((*list)->str);
-		free(*list);
-	}
+	free(curr->str);
+	free(curr);
 }
 
 t_fd_list	*get_line_by_fd(t_fd_list **list, int fd)
 {
 	t_fd_list	*new;
-
-	while (*list)
+	t_fd_list	*kr = *list;
+	while (kr->fd != -42)
 	{
-		if ((*list)->fd == fd)
-			return (*list);
-		if (!(*list)->next)
+		//print_list(list);
+		 printf("test1\n");
+		if (kr->fd == fd)
+			return (kr);
+		// printf("test2\n");
+		if (!(kr->next))
 			break;
-		*list = (*list)->next;
+		// printf("test3\n");
+		kr = kr->next;
+		// printf("test4\n");
 	}
+	// printf("test5\n");
 	if (!(new = (t_fd_list*)malloc(sizeof(t_fd_list))))
 		return (NULL);
+		// printf("tes4\n");
 	new->fd = fd;
 	new->str = ft_strnew(0);
 	new->next = NULL;
-	if (*list)
-		(*list)->next = new;
+	if (kr)
+		new->next = kr;
 	return (new);
 }
 
@@ -75,6 +83,7 @@ int		get_next_line(const int fd, char **line)
 	//print_list(&list);
 	if (!list)
 	{
+		// printf("lol\n");
 		if (!(list = (t_fd_list*)malloc(sizeof(t_fd_list))))
 			return (-1);
 		list->fd = -42;
@@ -97,9 +106,9 @@ int		get_next_line(const int fd, char **line)
 		while (curr->str[i] != '\n' && curr->str[i])
 			i++;
 		curr->str[i] = '\0';
-		if (i == 0)
+		/*if (i == 0)
 			*line = ft_strdup("");
-		else
+		else*/
 			*line = ft_strdup(curr->str);
 		tmp = ft_strdup(&(curr->str[i + 1]));
 		free(curr->str);
@@ -111,30 +120,42 @@ int		get_next_line(const int fd, char **line)
 			return (1);
 	}
 	//print_list(&list);
-	//delete_curr(&list, curr);
+	if (curr != NULL)
+		delete_curr(&list, curr);
 	//print_list(&list);
 	return (0);
 }
 
-/*int		main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	(void)argc;
 	char *str;
 	char *str2;
+	//int		i = 0;
 	int fd = open(argv[1], O_RDONLY);
 	int fd2 = open(argv[2], O_RDONLY);
+	/*get_next_line(fd, &str);
+	printf("GNL fd1 : %s\n", str);*/
+	get_next_line(fd2, &str2);
+	printf("GNL fd2 : %s\n", str2);
 	get_next_line(fd, &str);
 	printf("GNL fd1 : %s\n", str);
-	get_next_line(fd2, &str);
-	printf("GNL fd2 : %s\n", str);
+	get_next_line(fd2, &str2);
+	printf("GNL fd2 : %s\n", str2);
 	get_next_line(fd, &str);
 	printf("GNL fd1 : %s\n", str);
-	while (get_next_line(fd, &str))
+	get_next_line(fd2, &str2);
+	printf("GNL fd2 : %s\n", str2);
+	get_next_line(fd, &str);
+	printf("GNL fd1 : %s\n", str);
+	get_next_line(fd2, &str2);
+	printf("GNL fd2 : %s\n", str2);
+	get_next_line(fd, &str);
+	printf("GNL fd1 : %s\n", str);
+	/*while (get_next_line(fd, &str) || get_next_line(fd2, &str2))
 	{
-		printf("%s\n", str);
-		get_next_line(fd2, &str2);
-		printf("%s\n", str2);
-	}
+		printf("fd1 %d : \'%s\'\n", ++i, str);
+		printf("fd2 %d : \'%s\'\n", i, str2);
+	}*/
 	return (0);
 }
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 10:22:29 by abouvero          #+#    #+#             */
-/*   Updated: 2017/12/11 17:51:35 by abouvero         ###   ########.fr       */
+/*   Updated: 2017/12/13 11:48:02 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "libft/libft.h"
-# define BUFF_SIZE 128
+# define BUFF_SIZE 1
 
 #include <stdio.h>
 
